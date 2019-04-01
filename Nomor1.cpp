adfeb9b7f2aaaf441dec2e38e1e868ccc8de01b4
staisyah123 <49123201+staisyah123@users.noreply.github.com>
2019-04-01 13:05:38 +0800

    Create Nomor1.cpp

---
+++ Nomor1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  cout<<"nama :St.Aisyah"<<endl;
+  cout<<"Nim : 1829142025"<<endl;
+  cout<<"kelas : Teknik komputer B"<<endl;
+  cout<<"tahun angkatan: 2018"<<endl;
+  
+  return0;
+  }
